a310ac50f96102a0c4a235a6357b0e42b0d32f13
Andreas Pauley <apauley+github@gmail.com>
2015-05-03 00:26:30 +0200

    Allow more than just Int to be counted

--- src/Main.hs
+++ src/Main.hs
@@ -9,16 +9,17 @@ main = do
   let numbers = take 50000 $ randomIntStream seed
 
   putStrLn $ summary $ frequency numbers
+  putStrLn $ summary $ frequency "Hello, world!"
 
 frequency :: Ord a => [a] -> [(a, Int)]
 frequency xs = map (\l -> (head l, length l)) (group (sort xs))
 
-summary :: [(Int, Int)] -> String
+summary :: Show a => [(a, Int)] -> String
 summary freq = "Frequency counts:\n" ++ counters ++ "\n" ++ total
   where counters = foldl countStr "" freq
         total    = "Total: " ++ show (foldl (+) 0 $ map snd freq) ++ "\n"
 
-countStr :: String -> (Int, Int) -> String
+countStr :: Show a => String -> (a, Int) -> String
 countStr acc (a, count) = acc ++ show a ++ ":\t" ++ show count ++ "\n"
 
 randomIntStream :: StdGen -> [Int]
