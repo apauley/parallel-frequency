ffc631355b840535ef10efea335d7bdc7052bc13
Andreas Pauley <apauley+github@gmail.com>
2015-05-03 00:49:50 +0200

    List most frequent element first

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ import System.Random
 
 import Data.List
 
-type Count a = (a, Int)
+type Count a = (Int, a)
 type FrequencyCount a = [Count a]
 
 main :: IO ()
@@ -15,15 +15,15 @@ main = do
   putStrLn $ summary $ frequency "Hello, world!"
 
 frequency :: Ord a => [a] -> FrequencyCount a
-frequency xs = map (\l -> (head l, length l)) (group (sort xs))
+frequency as = reverse . sort $ map (\a -> (length a, head a)) (group (sort as))
 
 summary :: Show a => FrequencyCount a -> String
 summary freq = "Frequency counts:\n" ++ counters ++ "\n" ++ total
   where counters = foldl countStr "" freq
-        total    = "Total: " ++ show (foldl (+) 0 $ map snd freq) ++ "\n"
+        total    = "Total: " ++ show (foldl (+) 0 $ map fst freq) ++ "\n"
 
 countStr :: Show a => String -> Count a -> String
-countStr acc (a, count) = acc ++ show a ++ ":\t" ++ show count ++ "\n"
+countStr acc (count, a) = acc ++ show a ++ ":\t" ++ show count ++ "\n"
 
 randomIntStream :: StdGen -> [Int]
 randomIntStream = randomRs (1,5)
