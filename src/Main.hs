877bf71aca2cbc7a21cfbe328e3ae771e94e7556
Andreas Pauley <apauley+github@gmail.com>
2015-05-03 00:17:53 +0200

    Count frequency of some random numbers

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,25 @@
+import System.Random
+
+import Data.List
+
 main :: IO ()
 main = do
-  putStrLn "This program does not do anything useful. Yet. Time to start working..."
+  seed <- newStdGen
+
+  let numbers = take 50000 $ randomIntStream seed
+
+  putStrLn $ summary $ frequency numbers
+
+frequency :: Ord a => [a] -> [(a, Int)]
+frequency xs = map (\l -> (head l, length l)) (group (sort xs))
+
+summary :: [(Int, Int)] -> String
+summary freq = "Frequency counts:\n" ++ counters ++ "\n" ++ total
+  where counters = foldl countStr "" freq
+        total    = "Total: " ++ show (foldl (+) 0 $ map snd freq) ++ "\n"
+
+countStr :: String -> (Int, Int) -> String
+countStr acc (a, count) = acc ++ show a ++ ":\t" ++ show count ++ "\n"
+
+randomIntStream :: StdGen -> [Int]
+randomIntStream = randomRs (1,5)
