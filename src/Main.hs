543039761562147521eb65fdfda0eecf5aeae02f
Andreas Pauley <apauley+github@gmail.com>
2015-05-03 00:30:57 +0200

    Introduce some type aliases

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,9 @@ import System.Random
 
 import Data.List
 
+type Count a = (a, Int)
+type FrequencyCount a = [Count a]
+
 main :: IO ()
 main = do
   seed <- newStdGen
@@ -11,15 +14,15 @@ main = do
   putStrLn $ summary $ frequency numbers
   putStrLn $ summary $ frequency "Hello, world!"
 
-frequency :: Ord a => [a] -> [(a, Int)]
+frequency :: Ord a => [a] -> FrequencyCount a
 frequency xs = map (\l -> (head l, length l)) (group (sort xs))
 
-summary :: Show a => [(a, Int)] -> String
+summary :: Show a => FrequencyCount a -> String
 summary freq = "Frequency counts:\n" ++ counters ++ "\n" ++ total
   where counters = foldl countStr "" freq
         total    = "Total: " ++ show (foldl (+) 0 $ map snd freq) ++ "\n"
 
-countStr :: Show a => String -> (a, Int) -> String
+countStr :: Show a => String -> Count a -> String
 countStr acc (a, count) = acc ++ show a ++ ":\t" ++ show count ++ "\n"
 
 randomIntStream :: StdGen -> [Int]
