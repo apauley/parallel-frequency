a34e7836ed935f41443fc225f4b6653ddc363cde
Andreas Pauley <apauley+github@gmail.com>
2015-05-09 23:10:08 +0200

    Use div frequency

--- src/FrequencyMap.hs
+++ src/FrequencyMap.hs
@@ -9,22 +9,21 @@ import Shared hiding (frequency)
 type FrequencyMap a = Map.Map a Int
 
 frequency :: Ord a => [a] -> FrequencyCount a
-frequency = fromMap . frequencyMap
+frequency = fromMap . frequencyDiv
 
 fromMap :: Ord a => FrequencyMap a -> FrequencyCount a
 fromMap m = reverse . sort $ map swap (Map.toList m)
 
+frequencyDiv :: Ord a => [a] -> FrequencyMap a
+frequencyDiv = parFrequencyMap . splitList
+
+parFrequencyMap :: Ord a => ([a], [a]) -> FrequencyMap a
+parFrequencyMap (xs, ys) = Map.unionWith (+) m1 m2
+  where m1 = frequencyMap xs
+        m2 = frequencyMap ys
+
 frequencyMap :: Ord a => [a] -> FrequencyMap a
 frequencyMap xs = Map.fromListWith (+) [(x, 1) | x <- xs]
-
-frequencyDiv :: Ord a => [a] -> FrequencyCount a
-frequencyDiv as =
-  let
-    (xs,ys) = splitList as
-    m1 = frequencyMap xs
-    m2 = frequencyMap ys
-  in
-   fromMap $ Map.unionWith (+) m1 m2
 
 splitList :: [a] -> ([a], [a])
 splitList as = splitAt (length as `div` 2) as
