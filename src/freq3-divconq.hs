fcb60ba84c18ab9249602c9205d8d341aebb141e
Andreas Pauley <apauley+github@gmail.com>
2015-05-09 23:22:28 +0200

    Remove parCount from freq3 so that we can focus on the effects the algorithm par has

--- src/freq3-divconq.hs
+++ src/freq3-divconq.hs
@@ -2,10 +2,6 @@ import System.Environment (getArgs)
 import Data.Time (getCurrentTime)
 import System.Random
 
-import Control.Exception
-import Control.Parallel.Strategies
-import Control.DeepSeq
-
 import Shared hiding (frequency)
 import FrequencyMap
 
@@ -19,8 +15,9 @@ main = do
   putStrLn "Calculate frequency of elements in a list."
   t0 <- getCurrentTime
 
-  (wordFreq, charFreq) <- evaluate (parCount fileContents)
-  printTimeSince t0 "After parCount return."
+  let wordFreq = frequency (words fileContents)
+      charFreq = frequency fileContents
+  printTimeSince t0 "After freq return."
 
   printRandomNumFrequency seed frequency
   printTimeSince t0 "After num frequency print."
@@ -32,9 +29,3 @@ main = do
   putStrLn $ "\nTop 10 characters in " ++ fileName ++ ":"
   putStrLn $ summary (take 10 charFreq)
   printTimeSince t0 "After char frequency print."
-
-parCount :: String -> (FrequencyCount String, FrequencyCount Char)
-parCount fileContents = runEval $ do
-  w <- rpar $ force (frequency (words fileContents))
-  c <- rpar $ force (frequency fileContents)
-  return (w, c)
