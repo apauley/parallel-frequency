cd0b3da2ec7e5bc977178c0e60a4e449d316c039
Andreas Pauley <apauley+github@gmail.com>
2015-05-10 17:46:57 +0200

    Use deepseq force in parMap

--- src/FrequencyParMap.hs
+++ src/FrequencyParMap.hs
@@ -4,6 +4,7 @@ import Data.List.Split (chunksOf)
 import qualified Data.Map as Map
 
 import Control.Parallel.Strategies hiding (parMap)
+import Control.DeepSeq
 
 import FrequencyMap (fromMap, frequencyMap)
 
@@ -11,21 +12,21 @@ type Count a = (Int, a)
 type FrequencyCount a = [Count a]
 type FrequencyMap a = Map.Map a Int
 
-frequency :: Ord a => [a] -> FrequencyCount a
+frequency :: (NFData a, Ord a) => [a] -> FrequencyCount a
 frequency = fromMap . fold . frequencyMapMiniLists
 
-frequencyMapMiniLists :: Ord a => [a] -> [FrequencyMap a]
+frequencyMapMiniLists :: (NFData a, Ord a) => [a] -> [FrequencyMap a]
 frequencyMapMiniLists as = runEval $ parMap frequencyMap (splitList as)
 
-fold :: Ord a => [FrequencyMap a] -> FrequencyMap a
+fold :: (NFData a, Ord a) => [FrequencyMap a] -> FrequencyMap a
 fold = foldl (Map.unionWith (+)) Map.empty
 
 splitList :: [a] -> [[a]]
 splitList as = chunksOf 50 as
 
-parMap :: (a -> b) -> [a] -> Eval [b]
+parMap :: NFData b => (a -> b) -> [a] -> Eval [b]
 parMap _ [] = return []
 parMap f (a:as) = do
-   b <- rpar (f a)
+   b <- rpar $ force (f a)
    bs <- parMap f as
    return (b:bs)
