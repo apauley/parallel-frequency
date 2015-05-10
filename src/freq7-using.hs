117f5565e2c9e98212c3ff692195ca76fb34713d
Andreas Pauley <apauley+github@gmail.com>
2015-05-10 23:17:29 +0200

    Use parCount in freq7

--- src/freq7-using.hs
+++ src/freq7-using.hs
@@ -2,7 +2,11 @@ import System.Environment (getArgs)
 import Data.Time (getCurrentTime)
 import System.Random
 
-import Shared
+import Control.Exception
+import Control.Parallel.Strategies
+import Control.DeepSeq
+
+import Shared (printTimeSince, printRandomNumFrequency, summary)
 import FrequencyMapUsing
 
 main :: IO ()
@@ -15,9 +19,8 @@ main = do
   putStrLn "Calculate frequency of elements in a list."
   t0 <- getCurrentTime
 
-  let wordFreq = frequency (words fileContents)
-      charFreq = frequency fileContents
-  printTimeSince t0 "After freq return."
+  (wordFreq, charFreq) <- evaluate (parCount fileContents)
+  printTimeSince t0 "After parCount return."
 
   printRandomNumFrequency seed frequency
   printTimeSince t0 "After num frequency print."
@@ -29,3 +32,9 @@ main = do
   putStrLn $ "\nTop 10 characters in " ++ fileName ++ ":"
   putStrLn $ summary (take 10 charFreq)
   printTimeSince t0 "After char frequency print."
+
+parCount :: String -> (FrequencyCount String, FrequencyCount Char)
+parCount fileContents = runEval $ do
+  w <- rpar $ force (frequency (words fileContents))
+  c <- rpar $ force (frequency fileContents)
+  return (w, c)
