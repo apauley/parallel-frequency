a12cfb4d8f4f511219e3598165ef5130724263f9
Andreas Pauley <apauley+github@gmail.com>
2015-05-10 17:21:18 +0200

    FrequencyParMap

---
+++ src/FrequencyParMap.hs
@@ -0,0 +1,31 @@
+module FrequencyParMap where
+
+import Data.List.Split (splitEvery)
+import qualified Data.Map as Map
+
+import Control.Parallel.Strategies hiding (parMap)
+
+import FrequencyMap (fromMap, frequencyMap)
+
+type Count a = (Int, a)
+type FrequencyCount a = [Count a]
+type FrequencyMap a = Map.Map a Int
+
+frequency :: Ord a => [a] -> FrequencyCount a
+frequency = fromMap . fold . frequencyMapMiniLists
+
+frequencyMapMiniLists :: Ord a => [a] -> [FrequencyMap a]
+frequencyMapMiniLists as = runEval $ parMap frequencyMap (splitList as)
+
+fold :: Ord a => [FrequencyMap a] -> FrequencyMap a
+fold = foldl (Map.unionWith (+)) Map.empty
+
+splitList :: [a] -> [[a]]
+splitList as = splitEvery 50 as
+
+parMap :: (a -> b) -> [a] -> Eval [b]
+parMap f [] = return []
+parMap f (a:as) = do
+   b <- rpar (f a)
+   bs <- parMap f as
+   return (b:bs)
---
+++ src/freq6-parmap.hs
@@ -0,0 +1,31 @@
+import System.Environment (getArgs)
+import Data.Time (getCurrentTime)
+import System.Random
+
+import Shared
+import FrequencyParMap
+
+main :: IO ()
+main = do
+  [fileName] <- getArgs
+  seed       <- newStdGen
+
+  fileContents <- readFile fileName
+
+  putStrLn "Calculate frequency of elements in a list."
+  t0 <- getCurrentTime
+
+  let wordFreq = frequency (words fileContents)
+      charFreq = frequency fileContents
+  printTimeSince t0 "After freq return."
+
+  printRandomNumFrequency seed frequency
+  printTimeSince t0 "After num frequency print."
+
+  putStrLn $ "\nTop 10 words in " ++ fileName ++ ":"
+  putStrLn $ summary (take 10 wordFreq)
+  printTimeSince t0 "After word frequency print."
+
+  putStrLn $ "\nTop 10 characters in " ++ fileName ++ ":"
+  putStrLn $ summary (take 10 charFreq)
+  printTimeSince t0 "After char frequency print."
