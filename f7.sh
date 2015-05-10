31b654969d47cdf8eff612559a4be3e465d5f2e3
Andreas Pauley <apauley+github@gmail.com>
2015-05-10 18:10:04 +0200

    Use parList strategy with `using`

---
+++ f7.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+.cabal-sandbox/bin/freq7 ${@} +RTS -N4 -ls -s
---
+++ src/FrequencyMapUsing.hs
@@ -0,0 +1,24 @@
+module FrequencyMapUsing where
+
+import Data.List.Split (chunksOf)
+import qualified Data.Map as Map
+
+import Control.Parallel.Strategies
+
+import FrequencyMap (fromMap, frequencyMap)
+
+type Count a = (Int, a)
+type FrequencyCount a = [Count a]
+type FrequencyMap a = Map.Map a Int
+
+frequency :: (NFData a, Ord a) => [a] -> FrequencyCount a
+frequency = fromMap . fold . frequencyMapMiniLists
+
+frequencyMapMiniLists :: (NFData a, Ord a) => [a] -> [FrequencyMap a]
+frequencyMapMiniLists as = map frequencyMap (splitList as) `using` parList rseq
+
+fold :: (NFData a, Ord a) => [FrequencyMap a] -> FrequencyMap a
+fold = foldl (Map.unionWith (+)) Map.empty
+
+splitList :: [a] -> [[a]]
+splitList as = chunksOf 5000 as
