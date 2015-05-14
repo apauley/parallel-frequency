65903fc4368aaa3519b8948c01e9c8d2747a5b00
Andreas Pauley <apauley+github@gmail.com>
2015-05-14 22:30:53 +0200

    Speed up freq7 by using a strategy of (rpar . force)

--- src/FrequencyMapUsing.hs
+++ src/FrequencyMapUsing.hs
@@ -3,6 +3,7 @@ module FrequencyMapUsing where
 import qualified Data.Map as Map
 
 import Control.Parallel.Strategies
+import Control.DeepSeq
 
 import FrequencyMap (fromMap, frequencyMap)
 import Shared (split)
@@ -15,7 +16,7 @@ frequency :: (NFData a, Ord a) => [a] -> FrequencyCount a
 frequency = fromMap . fold . frequencyMapChunkedlist
 
 frequencyMapChunkedlist :: (NFData a, Ord a) => [a] -> [FrequencyMap a]
-frequencyMapChunkedlist as = map frequencyMap (split as) `using` parList rseq
+frequencyMapChunkedlist as = map frequencyMap (split as) `using` parList (rpar . force)
 
 fold :: (NFData a, Ord a) => [FrequencyMap a] -> FrequencyMap a
 fold = foldl (Map.unionWith (+)) Map.empty
