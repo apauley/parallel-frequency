627db95a1262bd68bf2a21b0a4200eb14c0d2e3b
Andreas Pauley <apauley+github@gmail.com>
2015-05-28 22:48:07 +0200

    Switch to character counting, it takes longer than word counting

--- src/DefaultMain.hs
+++ src/DefaultMain.hs
@@ -3,15 +3,15 @@ module DefaultMain where
 import System.Environment (getArgs)
 import Shared
 
-defaultMain :: (Show a, Ord a) => ([String] -> FrequencyCount a) -> IO ()
+defaultMain :: (Show a, Ord a) => ([Char] -> FrequencyCount a) -> IO ()
 defaultMain frequency = do
   t0           <- printT0
   [fileName]   <- getArgs
   fileContents <- readFile fileName
 
-  let wordFreq = frequency (words fileContents)
+  let charFreq = frequency fileContents
   printTimeSince t0 "After freq return."
 
-  putStrLn $ "\nTop 10 words in " ++ fileName ++ ":"
-  putStrLn $ summary (take 10 wordFreq)
-  printTimeSince t0 "After word frequency print."
+  putStrLn $ "\nTop 10 characters in " ++ fileName ++ ":"
+  putStrLn $ summary (take 10 charFreq)
+  printTimeSince t0 "After character frequency print."
