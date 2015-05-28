f2279ed367fc7e0d073aa93a5f21e5875b5588a3
Andreas Pauley <apauley+github@gmail.com>
2015-05-28 22:06:01 +0200

    Remove freq2, coarse grained parallelism not really interesting here

--- f2-par-datalist.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-THIS=$(basename ${0})
-echo "${THIS}: Initial attempt at coarse-grained parallelism."
-echo "${THIS}: Top-level words and character counts are done in parallel."
-echo "${THIS}: Same list-based algorithm as before."
-.cabal-sandbox/bin/freq2 ${@} +RTS -N2 -ls -s
