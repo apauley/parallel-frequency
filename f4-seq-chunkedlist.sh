48ebd2d9c24bc15df82eae2727ace0a7dc07021d
Andreas Pauley <apauley+github@gmail.com>
2015-05-31 22:11:03 +0200

    Update chunked description

--- f4-seq-chunkedlist.sh
+++ f4-seq-chunkedlist.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 THIS=$(basename ${0})
-echo "${THIS}: Sequential. Split list in chunks of 5000 so we can parallelise the chunks later."
+echo "${THIS}: Sequential. Split file into chunks so we can parallelise the chunks later."
+echo "${THIS}: Speedup mostly due to using Data.ByteString and Data.Text for chunking instead of lists."
 .cabal-sandbox/bin/freq-chunkedlist ${@} +RTS -ls -s
